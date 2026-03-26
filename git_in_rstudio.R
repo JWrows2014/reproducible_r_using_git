67455074d68da67825a8cfb7f9e7437e5b768274
joe.wu <wuz8@nih.gov>
2026-03-26 14:34:01 -0400

    Loaded mtcars data and calculated mean mpg for each cylinder.

--- git_in_rstudio.R
+++ git_in_rstudio.R
@@ -1,1 +1,9 @@
 # This script is used to demonstrate versioning using Git inside of RStudio.
+
+data(mtcars)
+
+# Take a look at the mtcars data
+View(mtcars)
+
+library(tidyverse)
+mtcars %>% group_by(cyl) %>% summarise(mean_mpg=mean(mpg))
